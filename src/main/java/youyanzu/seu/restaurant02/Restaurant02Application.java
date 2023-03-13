9b816865a0ac773586f8d99b81b49d25ac1ee554
CoderXshuai <xshuai1024@foxmail.com>
2023-03-13 15:05:50 +0800

    first

---
+++ src/main/java/youyanzu/seu/restaurant02/Restaurant02Application.java
@@ -0,0 +1,13 @@
+package youyanzu.seu.restaurant02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restaurant02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Restaurant02Application.class, args);
+    }
+
+}
---
+++ src/test/java/youyanzu/seu/restaurant02/Restaurant02ApplicationTests.java
@@ -0,0 +1,13 @@
+package youyanzu.seu.restaurant02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restaurant02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
