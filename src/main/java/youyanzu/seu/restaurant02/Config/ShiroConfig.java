34e76f7cdc8d3792e4c93f558265fdb4aeae08be
summerggg <summerwangx@163.com>
2023-03-15 13:48:21 +0800

    Config

--- src/main/java/youyanzu/seu/restaurant02/Config/ShiroConfig.java
+++ src/main/java/youyanzu/seu/restaurant02/Config/ShiroConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 @Configuration
 public class ShiroConfig {
 
